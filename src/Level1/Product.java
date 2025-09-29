d338eb987a79cef731ead6752c2756ea337307b8
Albert Medina <albertmecu@gmail.com>
2025-09-29 22:00:23 +0200

    fix: added getters for private attributes and added validations for constructors arguments

--- src/Level1/Product.java
+++ src/Level1/Product.java
@@ -6,8 +6,21 @@ public class Product {
     private double price;
 
     public Product(String name, double price) {
+
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid name");
+        }
+
+        if (price <= 0) {
+            throw new IllegalArgumentException("Invalid price");
+        }
+
         this.name = name;
         this.price = price;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public double getPrice() {
