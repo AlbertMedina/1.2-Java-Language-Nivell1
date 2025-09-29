fdd38153d5e17c37042b40e8fafd41e81ce1bc0e
Albert Medina <albertmecu@gmail.com>
2025-09-29 13:59:10 +0200

    feat: Implemented methods to read from scanner

---
+++ src/Level2/Main.java
@@ -0,0 +1,16 @@
+package Level2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Byte myByte = Input.readByte("");
+        int myInt = Input.readInt("");
+        float myFloat = Input.readFloat("");
+        double myDouble = Input.readDouble("");
+        char myChar = Input.readChar("");
+        String myString = Input.readString("");
+        Boolean myBoolean = Input.readYesOrNot("");
+
+        Input.closeScanner();
+    }
+}
